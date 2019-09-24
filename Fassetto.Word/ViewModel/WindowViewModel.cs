2d3cb5dc70771ad9ead492ee8cefbd82615ea8ee
anyuanlzh <anyuanlzh@qq.com>
2019-09-24 20:16:02 +0800

    2

--- Fassetto.Word/ViewModel/WindowViewModel.cs
+++ Fassetto.Word/ViewModel/WindowViewModel.cs
@@ -13,12 +13,61 @@ namespace Fassetto.Word.ViewModel
         /// </summary>
         private Window mWindow;
 
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        private int mOuterMarginSize = 10;
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        private int mWindowRadius = 10;
 
         #endregion
 
         #region Public Porperties
+        /// <summary>
+        /// The size of the resize border around the window
+        /// </summary>
+        public int ResizeBorder { get; set; } = 6;
 
-        public string Test { get; set; } = "Test String";
+        /// <summary>
+        /// The size of the resize border around the window, taking into account the outer margin
+        /// </summary>
+        public Thickness ResizeBorderThickness => new Thickness(ResizeBorder+OuterMarginSize);
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get => mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            set => mOuterMarginSize = value;
+        }
+
+        /// <summary>
+        /// The margin around the window to allow for a drop shadow
+        /// </summary>
+        public Thickness OuterMarginSizeThickniss => new Thickness(OuterMarginSize);
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        public int WindowRadius
+        {
+            get => mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            set => mWindowRadius = value;
+        }
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        public CornerRadius WindowCornerRadius => new CornerRadius(WindowRadius);
+
+        /// <summary>
+        /// The height of the title / caption of the window
+        /// </summary>
+        public int TitleHight { get; set; } = 42;
 
         #endregion
 
@@ -30,6 +79,17 @@ namespace Fassetto.Word.ViewModel
         public WindowViewModel(Window window)
         {
             mWindow = window;
+
+            // Listen out for the window resizing
+            mWindow.StateChanged += (sender, args) =>
+            {
+                // Fire off events for all properties that are affected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickniss));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
         }
 
         #endregion
