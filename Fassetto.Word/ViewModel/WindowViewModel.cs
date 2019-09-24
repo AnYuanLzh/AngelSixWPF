4d2dab033227308eb8b8a40dd60cc5d3eb588683
anyuanlzh <anyuanlzh@qq.com>
2019-09-24 19:34:22 +0800

    1

---
+++ Fassetto.Word/ViewModel/WindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace Fassetto.Word.ViewModel
+{
+    /// <summary>
+    /// The view model for the custom flat window
+    /// </summary>
+    class WindowViewModel:BaseViewModel
+    {
+        #region Priveate Member
+        /// <summary>
+        /// The window this view model controls
+        /// </summary>
+        private Window mWindow;
+
+
+        #endregion
+
+        #region Public Porperties
+
+        public string Test { get; set; } = "Test String";
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="window"></param>
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+        }
+
+        #endregion
+    }
+}
